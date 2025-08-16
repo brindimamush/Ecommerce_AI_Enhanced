75f207b497acf604f1e060ad258868b22116d8bd
brindimamush <brindimamush@gmail.com>
2025-08-17 00:53:01 +0300

    fixed error on import issues on auth.py

--- security/auth.py
+++ security/auth.py
@@ -5,12 +5,23 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+
+from sqlalchemy.orm import Session
+from database import database, models
+from database.models import User
+from pydantic import BaseModel
+
+# Define TokenData here instead of importing from main.py, as it's a generic Pydantic model
+class TokenData(BaseModel):
+    email: str | None = None
 
 # Security Configuration
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
 
 # Password Hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -47,9 +58,9 @@ def decode_access_token(token: str):
 
 #Dependency to get the current user from the token
 # This function will be used in routes to get the current user
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    from database import models # Import inside function to avoid circular imports
-    from main import TokenData # Import main.py
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    # from database import models # Import inside function to avoid circular imports
+    # from main import TokenData # Import main.py
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
